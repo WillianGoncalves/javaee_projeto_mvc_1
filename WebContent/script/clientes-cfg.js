4b9e34d191da0358f407c2807bbd2db4dfb6439e
Willian Gonçalves <willian_ig3@hotmail.com>
2016-11-14 22:28:30 -0200

    Create ControladoraFormCliente.

--- WebContent/script/clientes-cfg.js
+++ WebContent/script/clientes-cfg.js
@@ -1,11 +1,11 @@
 ( function( window, $, app, toastr ) {
 	'use strict';
-	
+
 	var configurar = function configurar() {
-		
+
 		console.log( window.location.hash );
 		var loc = window.location.hash;
-		var acao = '';		
+		var acao = '';
 		if ( /^\#\/clientes\/?$/.test( loc ) ) {
 			acao = 'listar';
 		} else if ( /^\#\/clientes\/[0-9]+\/alterar$/.test( loc ) ) {
@@ -13,31 +13,36 @@
 		} else if ( /^\#\/clientes\/novo$/.test( loc ) ) {
 			acao = 'novo';
 		}
-		
+
 		if ( 'listar' === acao ) {
 			$( '#titulo' ).html( 'Clientes' );
-			
+
 			$( '#cliente-form' ).hide();
 			$( '#cliente-listagem' ).show();
-			
+
 			var servCli = new app.ServicoClientes( $ );
-			var ctrl = new app.ControladoraListagemClientes(
-					servCli, $, toastr, app.router, window );
-			ctrl.configurar();			
+			var ctrl = new app.ControladoraListagemClientes(servCli, $, toastr, app.router, window );
+			ctrl.configurar();
+
 		} else if ( 'novo' === acao ) {
 			$( '#titulo' ).html( 'Novo Cliente' );
 			$( '#cliente-form' ).show();
 			$( '#cliente-listagem' ).hide();
-			
+
+			var servCli = new app.ServicoClientes($);
+			var servCid = new app.ServicoCidades($);
+			var ctrl = new app.ControladoraFormCliente(servCli, servCid, $, toastr);
+			ctrl.configurar();
+
 		} else if ( 'alterar' === acao ) {
 			var id = loc.match( /[0-9]+/ );
 			$( '#titulo' ).html( 'Cliente ' + id );
 			$( '#cliente-form' ).show();
-			$( '#cliente-listagem' ).hide();		
-		}		
-		
+			$( '#cliente-listagem' ).hide();
+		}
+
 	};
-	
+
 	$( window.document ).ready( configurar );
-	
+
 } )( window, jQuery, app, toastr );
