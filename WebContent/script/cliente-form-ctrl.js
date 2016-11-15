c2ff4db0c7840e6c999b8bf86ee941fa02a8f696
Willian Gonçalves <willian_ig3@hotmail.com>
2016-11-14 22:18:26 -0200

    Implement "cidade" list.

--- WebContent/script/cliente-form-ctrl.js
+++ WebContent/script/cliente-form-ctrl.js
@@ -1,31 +1,48 @@
 ( function( app ) {
 	'use strict';
-	
+
 	/**
 	 * Controladora de Formulário de Clientes
 	 */
-	function ControladoraFormCliente( servicoClientes, $, toastr ) {
-				
+	function ControladoraFormCliente( servicoClientes, servicoCidades, $, toastr ) {
+
 		var mostrarErro = function mostrarErro( jqXhr ) {
 			toastr.error( jqXhr.responseJSON.join( ", ") );
 		};
-		
+
 		var mostrarSucesso = function mostrarSucesso( data ) {
 			toastr.success( data.join( ", " ) );
 		};
-				
+
 		var registrarCliqueBotoes = function registrarCliqueBotoes() {
 		};
-		
+
 		var idSelecionado = function idSelecionado() {
 			return parseInt( $( '#clientes tbody .cor-linha :first' ).html() );
 		};
-		
+
+		var listarCidades = function listarcidades(data) {
+			console.log("listando cidades");
+			var html = '';
+
+			for ( var i in data) {
+				var cidade = data[i];
+
+				html += "<option value=" + cidade.id + ">" + cidade.nome + "</option>";
+			}
+
+			$('#cidade').html(html);
+		};
+
 		this.configurar = function configurar() {
 			registrarCliqueBotoes();
+
+			var promessa = servicoCidades.cidades();
+			promessa.done(listarCidades);
+			promessa.fail(mostrarErro);
 		};
 	}
-	
+
 	app.ControladoraFormCliente = ControladoraFormCliente;
-	
+
 } )( app );
